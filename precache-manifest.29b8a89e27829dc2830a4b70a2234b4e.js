bfea11915a88e2b56c1a73431a48978a7e21e6f0
Aaron Lin <aaron_lin@wistron.com>
2019-08-19 09:11:13 +0800

    Updates

---
+++ precache-manifest.29b8a89e27829dc2830a4b70a2234b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e382f9360f5d9b5e134196ad5948e10",
+    "url": "/wolf_judge/index.html"
+  },
+  {
+    "revision": "87dd7a23688c6eff8db6",
+    "url": "/wolf_judge/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c6103d8b08c34c0c8a36",
+    "url": "/wolf_judge/static/js/2.1836c0af.chunk.js"
+  },
+  {
+    "revision": "87dd7a23688c6eff8db6",
+    "url": "/wolf_judge/static/js/main.ac4cddc4.chunk.js"
+  },
+  {
+    "revision": "d718bb2690348fd71462",
+    "url": "/wolf_judge/static/js/runtime~main.c863cc3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wolf_judge/static/media/logo.5d5d9eef.svg"
+  }
+]);
